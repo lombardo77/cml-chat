6f96ac7bfa2ac7663d6098e9b0d336c62993a2a8
Nicholas Lombardo <lombrado.nicholas77@gmail.com>
2024-03-08 14:41:53 -0500

    first commit

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import threading as threads
+import os
+import signal
+import sys
+
+
+# listens for messages from the sever asynchronously
+# TODO: somehow make it so that the prompt symbol copies into a new line
+# when message is received
+def listener():
+    while True:
+        msg = s.recv(2048).decode("ascii")
+        print(f"\rserver: {msg}", flush=True)
+        print("> ", end="", flush=True)
+        sys.stdin.flush()
+
+
+# handles C-c by first sending "exit" to sever
+def handle_sigint(sig, frame):
+    s.send(b"exit")
+    os._exit(0)
+
+
+signal.signal(signal.SIGINT, handle_sigint)
+HOST = socket.gethostbyname("localhost")  # The server's hostname or IP address
+PORT = 19000  # The port used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    t = threads.Thread(target=listener)
+    t.start()
+    while True:
+        k = input("> ")
+        s.send(str.encode(k))
+        if k == "exit":
+            os._exit(0)
---
+++ tmchat.cpp
@@ -0,0 +1,114 @@
+#include <asm-generic/socket.h>
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <vector>
+#include <algorithm>
+
+#define PORT 19000
+#define SA struct sockaddr
+#define MAXLINE 2048
+
+using namespace std;
+
+// function prototypes
+void *handle_connection(void *args);
+void broadcast_msg(string msg, int senderfd);
+
+// structs
+typedef struct client_info {
+	int clientfd;
+	string ip_addr;
+} clinfo;
+
+// global variables
+vector<int> clients;
+
+int main (int argc, char *argv[]) {
+	int sockfd, clientfd;
+	struct sockaddr_in server;
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		std::cout << "socket failed\n";
+		exit(127);
+	}
+
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(PORT);
+	server.sin_family = AF_INET;
+
+	int one = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+
+	if (bind(sockfd, (SA*)&server, sizeof(server)) < 0) { 
+		cout << "bind failed\n";
+		exit(127);
+	}
+
+	if (listen(sockfd, 100) < 0) {
+		cout << "listen failed\n";
+		exit(127);
+	}
+
+	while (true) {
+		struct sockaddr_in clntaddr;
+		string ip_addr;
+		cout << "waiting for connections...\n";
+	
+		socklen_t clen = sizeof(clntaddr);
+		clientfd = accept(sockfd, (SA*)&clntaddr, &clen);
+		clients.push_back(clientfd);
+		
+		// once connected we send the client to a new thread
+		ip_addr = inet_ntoa(clntaddr.sin_addr);
+		cout << "We have a connection with: " << ip_addr << "\n";
+	
+		clinfo cl_info = {clientfd, ip_addr};
+		pthread_t thread;
+		pthread_create(&thread, NULL, handle_connection, &cl_info);
+
+	} 
+
+	return 0;
+}
+
+#define CAST_INFO( ptr ) ((clinfo *)ptr)
+
+void *handle_connection(void *args) {
+	int clientfd = CAST_INFO(args)->clientfd;
+	string ip_addr = CAST_INFO(args)->ip_addr;
+	string msg;
+	char client_buff[8 * (MAXLINE - 1)];
+
+	while (true) {
+		// read client msg and send to all other chatters
+		read(clientfd, client_buff, MAXLINE);
+		msg = client_buff;
+		if (msg == "exit") {
+			close(clientfd);
+			cout << ip_addr << " has left the chat\n";
+			clients.erase(remove(clients.begin(), clients.end(), clientfd), clients.end());
+			break;
+		}
+		
+		broadcast_msg(msg, clientfd);
+		cout << "<"<< ip_addr << "> " << msg << "\n";
+		memset(client_buff, 0, MAXLINE);
+		msg.clear();
+	}
+	return NULL;
+}
+
+void broadcast_msg(string msg, int senderfd) {
+	for (const int &i: clients) {
+		//if (senderfd != i)
+			write(i, msg.c_str(), msg.length() + 1);
+	}
+}
+
